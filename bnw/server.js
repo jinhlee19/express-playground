5613e3c8f244cb3b5f5f928757f11ea2e5283423
rafy <jinhlee19@gmail.com>
2021-12-24 23:55:21 +0900

    Express - Routes

--- bnw/server.js
+++ bnw/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    // console.log(req);
     res.send("<h1>Hello, World!</h1>");
 });
-// app.get() -> what should happen when our browser gets in touch with our server and makes a get request.
-// get(로컬 서버에 도착 시 get request is being sent to the route of our web site(location "/"), what should happen when you get it)
+
+app.get("/about", function(req,res){
+    res.send("<h3>I am Rafy.</h3>")
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
-// callback function that can give sign when server is running
