7f17d1bf6ad24c8575201803d8f50d93b148f5a5
rafy <jinhlee19@gmail.com>
2022-03-03 03:05:19 +0900

    Step 3

---
+++ playground/pr-1/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, { useNewUrlParser: true });
+		console.log('Mongodb Connected');
+	} catch (err) {
+		console.log(err.message);
+		// 1: 즉시 종료 
+		process.exit(1);
+	}
+};
+console.log(db);
+module.exports = connectDB;
