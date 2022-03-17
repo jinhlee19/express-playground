9b4ea72f7ec4c8428335adaf768710f1513bef67
rafy <jinhlee19@gmail.com>
2022-03-17 18:24:55 +0900

    6 - bcrypt

--- jskr/routes/api/users.js
+++ jskr/routes/api/users.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
-// Validator
+const config = require('config');
+// gravatar, bcrypt, jwt
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
+const User = require('../../models/Users');
+
 
 //// REGISTER ROUTE 만드는 시간 ~
 
@@ -20,22 +25,63 @@ router.post(
 		}),
 	],
 	async (req, res) => {
-		const error = validationResult(req);
+		const errors = validationResult(req);
 		// validationResult는 req로 넘어오는 값으로 body()의 유효성을 검증 후, 에러 발생시 배열로 반환
-		if (!error.isEmpty()) {
-			return res.status(400).json({ errors: error.array() });
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
 		}
-		let user = User.findOne({ email });
+		const { name, email, password } = req.body;
+		
 		try {
+			let user = await User.findOne({ email });
 			if (user) {
-				console.log(user);
+				// console.log(user);
 				return res
 					.status(400)
-					.json({ errors: [{ msg: '이미 등록된 이메일입니다.' }] });
+					.json({ error: [{ msg: '이미 등록된 이메일입니다.' }] });
 			}
-			// 여기서부터 아바타, bcrypt, jwt
-			
-		} catch (error) {
+			////  ****** 여기서부터 아바타, bcrypt, JWT
+			// Avatar
+			const avatar = gravatar.url(email, {
+				// default size
+				s: '200',
+				r: 'pg',
+				// default image
+				d: 'mm',
+			});
+			// USER MODEL ** = 36열
+			user = new User({
+				name,
+				email,
+				avatar,
+				password,
+			});
+
+			// Bcrypt - bcrypt.genSalt는 promise를 반환함. 모든 promise를 반환하는 것들에 await을 붙인다.
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			// user를 DB에 저장 / REGISTER . - .save()도 promise 반환.
+			await user.save();
+
+			res.send('user Registered');
+
+			// ****** JWT ******
+			// JWT Payload
+			// const payload = {
+			// 	user: {
+			// 		id: user.id,
+			// 	},
+			// };
+			// jwt.sign(
+			// 	payload,
+			// 	config.get('jwtSecret'),
+			// 	{ expiresIn: 360000 },
+			// 	(err, token) => {
+			// 		if (err) throw err;
+			// 		res.json({ token });
+			// 	}
+			// );
+		} catch (err) {
 			console.error(err.message);
 			res.status(500).send('서버 오류');
 		}
