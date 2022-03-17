e0aae000499455247e94c773426d9129c8c6263e
rafy <jinhlee19@gmail.com>
2022-03-17 11:55:29 +0900

    1 setting express routes

---
+++ jskr/routes/api/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @ Route      Get api/auth
+// @ Desc       Test Route
+// @ Access     Public
+
+router.get('/', (req, res) => {
+	res.send('Auth Route');
+});
+
+module.exports = router;
--- jskr/server.js
+++ jskr/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const app = express();
-
-// Connect DB
 const connectDB = require('./config/db');
+const app = express();
 connectDB();
 
+// Connect DB
+
+// Q) 이때, app.get 과 app.use의 차이?
 app.get('/', (req,res)=>{
     res.send('Api running!');
 });
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
 const PORT = process.env.PORT || 5003;
 app.listen(PORT, ()=>{
     console.log(`server started on PORT ${PORT}`)
