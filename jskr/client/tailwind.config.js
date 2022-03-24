4d946b2e028baf48e5be33672d74727586c6803e
rafy <jinhlee19@gmail.com>
2022-03-24 23:24:46 +0900

    Tailwind Setup 2

--- jskr/client/tailwind.config.js
+++ jskr/client/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		fontFamily: {
+			sans: ['SUIT', 'ui-sans-serif', 'system-ui'],
+			serif: ['SUIT', 'ui-serif', 'Georgia'],
+			mono: ['SUIT', 'ui-monospace', 'SFMono-Regular'],
+			display: ['Jalnan'],
+			body: ['SUIT'],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
