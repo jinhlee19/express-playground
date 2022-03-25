8092e9af461f0e2fe071de405970a7119a22da8e
rafy <jinhlee19@gmail.com>
2022-03-25 18:57:16 +0900

    React Router Setup 1

--- jskr/client/src/App.js
+++ jskr/client/src/App.js
@@ -1,11 +1,26 @@
+import React from 'react'
 import './App.css';
 
+// Router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Layout
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+// Redux
+
 function App() {
-  return (
-    <div className="App">
-      <h1 className='text-4xl text-sky-400'>Hello world</h1>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ jskr/client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div className="">
+			<h1 className="text-4xl font-semibold">하이</h1>
+		</div>
+	);
+};
+
+export default Landing;
